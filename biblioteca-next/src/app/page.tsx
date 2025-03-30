9408e2b94f8d519865048b52ad538e2558060715
Kaik Bastos <kaikbastos@id.uff.br>
2025-03-30 20:00:46 -0300

    (feat): Adicionado e estilizado a página de login, o layout global e estilizado os mesmos

--- biblioteca-next/src/app/page.tsx
+++ biblioteca-next/src/app/page.tsx
@@ -1,95 +1,71 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
 
-export default function Home() {
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import styles from './page.module.css';
+import logo from '@/assets/Logo.svg';
+import picture from '@/assets/Picture.png';
+import Image from 'next/image';
+
+const schema = z.object({
+  email: z.string().nonempty('Por favor, insira um email válido!').email('Por favor, insira um email válido!'),
+  password: z.string().nonempty('A senha deve ter no mínimo 6 caracteres!').min(6, 'A senha deve ter no mínimo 6 caracteres!'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export default function LoginForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const router = useRouter();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+
+    router.push('/home');
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.loginFormContainer}>
+      <div className={styles.imageContainer}>
+        <Image src={picture} alt="login-illustration" />
+      </div>
+      <div className={styles.formContainer}>
+        <Image src={logo} alt="site-logo" className={styles.logo} />
+        <div className={styles.greetings}>
+          <h2>Bem-vindo(a)!</h2>
+          <h1>Entre na sua conta</h1>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <label htmlFor="email" className={styles.label}>E-mail</label>
+          <input
+            type="email"
+            id="email"
+            placeholder='Digite aqui seu e-mail'
+            {...register('email')}
+            className={styles.loginInput}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+
+          <label htmlFor="password" className={styles.label}>Senha</label>
+          <input
+            type="password"
+            id="password"
+            placeholder='Digite aqui sua senha'
+            {...register('password')}
+            className={styles.loginInput}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          {errors.password && <p className={styles.error}>{errors.password.message}</p>}
+
+          <div className={styles.btns}></div>
+          <button type="submit" className={styles.loginButton}>Entrar</button>
+          <button type="button" className={styles.registerButton}>Cadastre-se</button>
+        </form>
+      </div>
     </div>
   );
 }
